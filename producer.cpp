a69efffddbc40b592d47f8d34a278a169c9db037
Garrett <garrettohara2018@gmail.com>
2022-04-17 18:46:04 -0700

    push from mac

--- producer.cpp
+++ producer.cpp
@@ -18,7 +18,7 @@ void* producer::produce(void* args){
     while(true){
         i++;
     // for(int i = 0; i < 10; i++){
-        sleep(0.5);
+        sleep(1);
         sem_wait(DATA->mutex);
 
         /* CRITICAL SECTION */
